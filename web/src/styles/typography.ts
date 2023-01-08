508d7cb164fe4f6cd0ee4824e9af3c47fc46bc8e
Felipe Pessoa <felipedeazevedopessoa@gmail.com>
2023-01-08 20:07:59 -0300

    feat(web): add typography components

---
+++ web/src/styles/typography.ts
@@ -0,0 +1,29 @@
+import styled from 'styled-components';
+
+interface HeadingProps {
+  size?: 'xs' | 's' | 'l';
+  color?: 'gray-500' | 'gray-700' | 'gray-900';
+  weight?: '500' | '700';
+  lineHeight?: '130' | '150';
+}
+
+interface TextProps {
+  size?: 'xs' | 's' | 'm';
+  color?: 'gray-500' | 'gray-600' | 'gray-700' | 'gray-900' | 'purple-600';
+  weight?: '400' | '500' | '700';
+  lineHeight?: '110' | '150';
+}
+
+export const Heading = styled.h1<HeadingProps>`
+  color: ${({ theme, color }) => theme.colors[`${color ?? 'gray-900'}`]};
+  font-size: ${({ theme, size }) => theme.fontSizes[`${size ?? 'l'}`]};
+  line-height: ${({ lineHeight }) => `${lineHeight ?? '150'}%`};
+  font-weight: ${({ weight }) => weight ?? '700'};
+`;
+
+export const Text = styled.p<TextProps>`
+  color: ${({ theme, color }) => theme.colors[`${color ?? 'gray-900'}`]};
+  font-size: ${({ theme, size }) => theme.fontSizes[`${size ?? 's'}`]};
+  line-height: ${({ lineHeight }) => `${lineHeight ?? '150'}%`};
+  font-weight: ${({ weight }) => weight ?? '500'};
+`;
